15f17c23c5e53807a8d05bf6d05664bd3db7ca30
domfinch <finch.163@buckeyemail.osu.edu>
2022-05-03 17:10:24 -0400

    updated challenge 4

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+// global variables
+var timerEl = document.querySelector('#timer');
+var startcontainer = document.querySelector('#start')
+var startbutton = document.querySelector('#start-quiz')
+var questions = document.querySelector('#questions')
+var question = document.querySelector('#question')
+var questionbutton = document.querySelector('#question-button')
+var done = document.querySelector('#done')
+var score = document.querySelector('#score')
+var initials = document.querySelector('#initials')
+var submit = document.querySelector('#submit')
+var timerId;
+
+var questionArr = [
+    {
+        question: "what is 2 + 2?",
+        answers: ["1","2","3","4"],
+        correctanswer: "4"
+    },
+    {
+        question: "what is 1 + 2?",
+        answers: ["1","2","3","4"],
+        correctanswer: "3"
+    },
+    {
+        question: "what is 2 + 2?",
+        answers: ["1","2","3","4"],
+        correctanswer: "4"
+    },
+    {
+        question: "what is 1 + 1?",
+        answers: ["1","2","3","4"],
+        correctanswer: "2"
+    },
+    {
+        question: "what is 3 + 1?",
+        answers: ["1","2","3","4"],
+        correctanswer: "4"
+    },
+]
+
+var index = 0
+var time = 60;
+
+
+//functions
+
+function start(){
+//start button clicked, start the timer, hide the start container, display the question container (remove the class hide), display the first question
+timerEl.textContent = time;
+
+timerId = setInterval(function(){
+time--;
+
+timerEl.textContent = time;
+
+}, 1000);
+
+// look into setAttribute and removeAttribute to add and remove the hide class
+
+}lki3.;lvgbhgb
+//event listeners
+startbutton.addEventListener('click', start)
